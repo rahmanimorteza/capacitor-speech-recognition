fa21254b919b8a1017351275060dc60b68e86902
Morteza Rahmani <68592261+rahmanimorteza@users.noreply.github.com>
2025-12-07 19:17:45 +0330

    Clean up unused imports in SpeechRecognition.java
    Removed unused imports for Handler and Looper.

--- android/src/main/java/com/getcapacitor/community/speechrecognition/SpeechRecognition.java
+++ android/src/main/java/com/getcapacitor/community/speechrecognition/SpeechRecognition.java
@@ -5,8 +5,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
